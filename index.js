d66b82520cd5d778673acafbdcbe2197f61e3a0e
Johan von Boer <johan.von.boer@umu.se>
2021-08-20 12:25:41 +0200

    first version

---
+++ index.js
@@ -0,0 +1,89 @@
+#!/usr/bin/node
+
+const { MongoClient } = require('mongodb');
+
+if(!process.env.MONGO_ROOT_PASSWORD) {
+    console.log("This tool needs to have the mongo pass provided as the envvar MONGO_ROOT_PASSWORD");
+    return;
+}
+
+let cmd = process.argv[2];
+
+if(!cmd) {
+    console.log("Commands: add <user eppn>, del <user eppn>, list");
+    return;
+}
+
+let userEppn = process.argv[3];
+
+switch(cmd) {
+    case "add":
+        if(!userEppn) {
+            console.log("No user eppn provided!");
+            return;
+        }
+        addUser(userEppn);
+        break;
+
+    case "del":
+        if(!userEppn) {
+            console.log("No user eppn provided!");
+            return;
+        }
+        delUser(userEppn);
+        break;
+
+    case "list":
+        listUsers();
+        break;
+}
+
+function addUser(userEppn) {
+    connectToMongo().then((mongoClient) => {
+        let db = mongoClient.db("humlab_speech");
+        const usersCollection = db.collection("users");
+        usersCollection.insertOne({
+            "eppn": userEppn
+        }).then((result) => {
+            console.log(result);
+            mongoClient.close();
+        });
+    });
+}
+
+function delUser(userEppn) {
+    connectToMongo().then((mongoClient) => {
+        let db = mongoClient.db("humlab_speech");
+        const usersCollection = db.collection("users");
+        usersCollection.deleteOne({
+            "eppn": userEppn
+        }).then((result) => {
+            console.log(result);
+            mongoClient.close();
+        });
+    });
+}
+
+function listUsers() {
+    connectToMongo().then((mongoClient) => {
+        let db = mongoClient.db("humlab_speech");
+        const usersCollection = db.collection("users");
+        console.log("Users:");
+        usersCollection.find({}).toArray().then((usersList) => {
+            usersList.forEach(userObj => {
+                console.log(userObj.eppn);
+            });
+
+            mongoClient.close();
+        });
+        
+    });
+    
+}
+
+async function connectToMongo() {
+    const mongoPass = process.env.MONGO_ROOT_PASSWORD;
+    const mongodbUrl = 'mongodb://root:'+mongoPass+'@localhost:27017';
+    const mongoClient = new MongoClient(mongodbUrl);
+    return await mongoClient.connect();
+}
